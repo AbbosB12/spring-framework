9c57b6179ffba849684c6b507afdbfe18d3ee11f
Abbosbek123 <azamatabbos@gmail.com>
2022-02-17 20:59:13 -0500

    Motorcycyle Beans class

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/Motorcycyle.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class Motorcycyle {
+
+    private String model;
+
+}

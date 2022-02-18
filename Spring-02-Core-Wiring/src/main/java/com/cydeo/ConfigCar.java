ecd7fab248bd2d6a40b8bdfb1bba3b1261707b54
Abbosbek123 <azamatabbos@gmail.com>
2022-02-17 20:59:42 -0500

    ConfigCar class

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/ConfigCar.java
@@ -0,0 +1,38 @@
+package com.cydeo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigCar {
+
+    @Bean
+    Car car(){
+        Car car = new Car();
+        car.setMake("Porshe");
+        return car;
+    }
+    @Bean
+    Motorcycyle motorcycyle(){
+        Motorcycyle motorcycyle = new Motorcycyle();
+        motorcycyle.setModel("Yams");
+        return motorcycyle;
+    }
+    //Direct wiring
+//@Bean
+//    Person person(){
+//        Person p = new Person();
+//        p.setName("Mike");
+//        p.setCar(car());
+//        return p;
+//    }
+//Auto writing
+    @Bean
+    Person person(Car car, Motorcycyle motorcycyle){
+        Person p = new Person();
+        p.setName("Mike");
+        p.setCar(car);
+        p.setMotorcycyle(motorcycyle);
+        return p;
+    }
+}

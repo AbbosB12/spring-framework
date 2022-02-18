f59b7951a1ff9697b8e17ef8579918c3013b376f
Abbosbek123 <azamatabbos@gmail.com>
2022-02-17 20:59:55 -0500

    DealerApp class

---
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/DealerApp.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DealerApp {
+    public static void main(String[] args) {
+        ApplicationContext container= new AnnotationConfigApplicationContext(ConfigCar.class);
+
+        Car car = container.getBean(Car.class);
+        Person person = container.getBean(Person.class);
+        Motorcycyle motorcycyle=container.getBean(Motorcycyle.class);
+
+        System.out.println("Person's name : " + person.getName());
+        System.out.println("Car's make : " + car.getMake());
+        System.out.println("Persons's car : " + person.getCar().getMake());
+        System.out.println("Person's motorcycle : "+person.getMotorcycyle().getModel());
+    }
+}

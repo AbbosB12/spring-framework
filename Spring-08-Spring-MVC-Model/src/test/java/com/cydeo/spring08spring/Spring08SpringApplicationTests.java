6f70143b80fe8c3114449c9f6997938429c63608
Rasul <abbosboev@gmail.com>
2022-03-01 21:25:34 -0500

    Create first web application on Spring

---
+++ Spring-08-Spring-MVC-Model/src/test/java/com/cydeo/spring08spring/Spring08SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring08spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring08SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

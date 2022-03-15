68e10eb0c74f9ef96edc253c165c564f489f7cb3
Abbos Shirinboev <abbosboev@gmail.com>
2022-03-15 19:50:41 -0400

    Create one endpoint

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -15,5 +15,9 @@ public class StudentController {
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
+    @RequestMapping("/welcome")
+    public String info(){
+        return "student/welcome";
+    }
 
 }

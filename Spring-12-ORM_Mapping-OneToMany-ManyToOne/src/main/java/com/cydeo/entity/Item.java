348f61f4aa1044dd23aab037e1a26a9f9943f31c
Abbos Shirinboev <abbosboev@gmail.com>
2022-04-21 21:15:20 -0400

    Item entity is created

---
+++ Spring-12-ORM_Mapping-OneToMany-ManyToOne/src/main/java/com/cydeo/entity/Item.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Table(name = "items")
+@Data
+@NoArgsConstructor
+public class Item extends BaseEntity {
+
+    private String name;
+    private String code;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Cart>cart;
+
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+}

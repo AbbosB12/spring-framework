ce15c3e2a4ad0883c640ed01d37c6ae858e3c88a
Abbos Shirinboev <abbosboev@gmail.com>
2022-06-02 20:17:46 -0400

    Create Rest and Jackson module

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController // @Controller + @ResponseBody
-@RequestMapping("/courses")
+@RequestMapping("/courses/api/v1")
 public class CourseController {
 
     private final CourseService courseService;
@@ -28,6 +28,34 @@ public class CourseController {
     public CourseDTO getCourseById(@PathVariable("id") Long courseId){
         return courseService.getCourseById(courseId);
     }
+
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
+
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId,@RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    public void deleteCourses(){
+        courseService.deleteCourses();
+    }
+
+
 
 
 
